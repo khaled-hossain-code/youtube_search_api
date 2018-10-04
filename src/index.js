029b1c7ed9a05d9ec01c7a36b5ad46c73c888882
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-10-04 20:10:38 +0600

    import keys

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import {API_KEY} from './config/keys'; 
+import keys from './config/keys'; 
+
 import SearchBar from './components/SearchBar';
 
+YTSearch({
+  key: keys.API_KEY,
+  term: 'surfboards'
+}, data => console.log(data))
 
 const App = () => {
   return(
